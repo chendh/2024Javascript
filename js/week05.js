ceac179dcd36a9634eeccc2d469700dc755c3103
陳定宏(Ding-Horng Chen) <chendh@stust.edu.tw>
2024-03-20 12:00:07 +0800

    Week05 demo

---
+++ js/week05.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    let x = document.getElementById("demo");
+    const str = "Hello World";
+    x.innerHTML = str;
+    x.style.color = "red";
+    x.style.backgroundColor = "yellow";
+}
