deefd8d78bc72da85a77606600046b1c7bfbef86
陳定宏(Ding-Horng Chen) <chendh@stust.edu.tw>
2024-03-21 16:38:47 +0800

    Week05 demo

---
+++ js/week05.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    // document.getElementById('demo').innerHTML = 'Hello Javascript';
+    // document.getElementById('demo').style.color = 'red';
+    let x = 5.1234859859358;
+    let y = x.to(2);
+    document.getElementById('demo').innerHTML = y;
+}
